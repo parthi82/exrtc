7847d2e589a20d42389e26efd328bf5f739d8baa
parthibhan kp <parthi.kannan@a-cti.com>
2017-03-05 23:40:49 +0530

    changes for heroku

--- web/channels/user_socket.ex
+++ web/channels/user_socket.ex
@@ -5,7 +5,8 @@ defmodule Exrtc.UserSocket do
   # channel "room:*", Exrtc.RoomChannel
 
   ## Transports
-  transport :websocket, Phoenix.Transports.WebSocket
+  transport :websocket, Phoenix.Transports.WebSocket,
+    timeout: 45_000
   # transport :longpoll, Phoenix.Transports.LongPoll
 
   # Socket params are passed from the client and can
